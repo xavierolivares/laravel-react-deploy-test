f547c9e30b483066bca8b9c2ec3e33861099242f
Xavier Olivares <xm.olivares@gmail.com>
2020-07-01 15:18:16 -0400

    minor changes to App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Jungle
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.xavierolivares.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portfolio Site
         </a>
       </header>
     </div>
